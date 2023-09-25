6f3607bf4ae86bc7ad33da2e33926cf29d060001
Pedro Lucas Dall' Igna <pedro.lucas-di@hotmail.com>
2023-09-24 22:49:16 -0300

    fix: passes pool to request instanciation

--- src/models/Equipes/Equipes.ts
+++ src/models/Equipes/Equipes.ts
@@ -1,6 +1,6 @@
 import mssql, { ConnectionPool } from "mssql";
 
-interface IEquipes {
+interface IEquipe {
   nome: string,
   descricao: string,
   area: string
@@ -24,8 +24,8 @@ class Equipes {
     return pool.query(query);
   }
 
-  public async insertEquipe(pool: ConnectionPool, data: IEquipes) {
-    const request = new mssql.Request();
+  public async insertEquipe(pool: ConnectionPool, data: IEquipe) {
+    const request = new mssql.Request(pool);
     request.input('Nome', mssql.NVarChar(60), data.nome);
     request.input('Descricao', mssql.NVarChar(300), data.descricao);
     request.input('AreaNome', mssql.NVarChar(60), data.area);
@@ -33,8 +33,8 @@ class Equipes {
     return request.execute('InserirEquipe');
   }
 
-  public async updateEquipe(pool: ConnectionPool, id: number | string, data: IEquipes) {
-    const request = new mssql.Request();
+  public async updateEquipe(pool: ConnectionPool, id: number | string, data: IEquipe) {
+    const request = new mssql.Request(pool);
     request.input('Id_Equipe', mssql.Int, Number(id));
     request.input('Nome', mssql.NVarChar(60), data.nome);
     request.input('Descricao', mssql.NVarChar(300), data.descricao);
@@ -44,7 +44,7 @@ class Equipes {
   }
 
   public async deleteEquipe(pool: ConnectionPool, id: number | string) {
-    const request = new mssql.Request();
+    const request = new mssql.Request(pool);
     request.input('Id', mssql.Int, Number(id));
 
     return request.execute('ExcluirEquipe');
